024bf5215dd3ded95a935540e0fcd5aae9203647
zhzahid1911509 <zahid.hasan.nsu2019@gmail.com>
2023-03-02 14:33:54 +0600

    more files added

---
+++ Inventory Management System/packages/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagementSystem.Models;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserData obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                CRUDAppDBEntities db = new CRUDAppDBEntities();
+                db.UserDatas.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
